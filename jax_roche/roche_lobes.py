b4dc49b9029a84d32362945f69d49dd0656e33a7
Stuart Littlefair <s.littlefair@shef.ac.uk>
2022-10-25 12:50:48 +0100

    remove experimental loops

--- jax_roche/roche_lobes.py
+++ jax_roche/roche_lobes.py
@@ -3,14 +3,13 @@ from .methods import rtsafe
 from .potentials import rpot1, rpot2, rpot
 from .vector import Vec3
 from .types import Star
+from jax.lax import cond, fori_loop
 from jax import numpy as jnp
 from jax import grad, jit
 from functools import partial
 
 
-from jax.experimental import loops
-
-
+@jit
 def rlobe_eggleton(q):
     """
     The volume-averaged Roche lobe radius, as defined by Peter Eggelton's formula.
@@ -30,7 +29,8 @@ def rlobe_eggleton(q):
     return 0.49 * q3 * q3 / (0.6 * q3 * q3 + jnp.log(1 + q3))
 
 
-def ref_sphere(q: float, star: Star, spin: float, ffac: float):
+@jit
+def ref_sphere(q: float, star: int, spin: float, ffac: float):
     """
     Computes the radius of a reference sphere just touching a Roche distorted star.
 
@@ -44,8 +44,8 @@ def ref_sphere(q: float, star: Star, spin: float, ffac: float):
     q: float, `jnp.DeviceArray`
         Mass ratio M2/M1
 
-    star: `jax_roche.types.Star`
-        Compute for primary or secondary star?
+    star: int
+        Compute for primary (1) or secondary star (2)?
 
     spin: float, `jnp.DeviceArray`
         spin = ratio of angular/orbital frequency of primary/secondary
@@ -62,27 +62,50 @@ def ref_sphere(q: float, star: Star, spin: float, ffac: float):
         Radius of the reference sphere
 
     pref: float,  `jnp.DeviceArray`
-        The reference potential. This is the Roche potential on surface of 
+        The reference potential. This is the Roche potential on surface of
         the distorted star.
     """
-    if star == Star.PRIMARY:
+
+    def star1():
         rref = ffac * xl11(q, spin)
         pref = rpot1(q, spin, Vec3(rref, 0.0, 0.0))
-    if star == Star.SECONDARY:
+        return rref, pref
+
+    def star2():
         rref = ffac * (1 - xl12(q, spin))
         pref = rpot2(q, spin, Vec3(1 - rref, 0.0, 0.0))
-    return rref, pref
+        return rref, pref
+
+    return cond(star == 1, star1, star2)
 
 
 @jit
 def lobe2(q, n=200):
+    """
+    Return arrays x and y for plotting an equatorial section of the secondary's Roche Lobe
+
+    The arrays start and end at the inner lagrangian point and are evenly spaced in
+    azimuth looking from the centre of mass of the secondary star.
+
+    Parameters
+    ----------
+    q: float, `jnp.DeviceArray`
+        Mass ratio M2/M1
+
+    n: in, `jnp.DeviceArray`, default=200
+        Number of points around lobe
+
+    Returns
+    -------
+    x, y: `jnp.DeviceArray`
+        x and y coordinates of secondary's Roche Lobe
+    """
 
     rl1 = xl1(q)
     cpot = rpot(q, Vec3(rl1, 0.0, 0.0))
     upper = 1.0 - rl1
     lower = upper / 4
 
-    @jit
     def root_func(t, dx, dy):
         """
         t is a step in direction dx, dy
@@ -90,18 +113,71 @@ def lobe2(q, n=200):
         p = Vec3(1.0 + t * dx, t * dy, 0.0)
         return rpot(q, p) - cpot
 
-    gradf = jit(grad(root_func))
-    with loops.Scope() as s:
-        s.x = jnp.zeros(n)
-        s.y = jnp.zeros(n)
-        for i in s.range(n):
-            theta = 2.0 * jnp.pi * i / (n - 1)
-            dx = -jnp.cos(theta)
-            dy = jnp.sin(theta)
+    gradf = grad(root_func)
 
-            f = partial(root_func, dx=dx, dy=dy)
-            df = partial(gradf, dx=dx, dy=dy)
-            rad = rtsafe(f, df, lower, upper)
-            s.x = s.x.at[i].set(1.0 + rad * dx)
-            s.y = s.y.at[i].set(rad * dy)
-    return s.x, s.y
+    def loop_body(i, state):
+        theta = 2.0 * jnp.pi * i / (n - 1)
+        dx = -jnp.cos(theta)
+        dy = jnp.sin(theta)
+        f = partial(root_func, dx=dx, dy=dy)
+        df = partial(gradf, dx=dx, dy=dy)
+        rad = rtsafe(f, df, lower, upper)
+        state["x"] = state["x"].at[i].set(1.0 + rad * dx)
+        state["y"] = state["y"].at[i].set(rad * dy)
+        return state
+
+    init_state = dict(x=jnp.zeros(n), y=jnp.zeros(n))
+    end_state = fori_loop(0, n, loop_body, init_state)
+    return end_state["x"], end_state["y"]
+
+
+@jit
+def lobe1(q, n=200):
+    """
+    Return arrays x and y for plotting an equatorial section of the primary's Roche Lobe
+
+    The arrays start and end at the inner lagrangian point and are evenly spaced in
+    azimuth looking from the centre of mass of the primary star.
+
+    Parameters
+    ----------
+    q: float, `jnp.DeviceArray`
+        Mass ratio M2/M1
+
+    n: in, `jnp.DeviceArray`, default=200
+        Number of points around lobe
+
+    Returns
+    -------
+    x, y: `jnp.DeviceArray`
+        x and y coordinates of primary's Roche Lobe
+    """
+    rl1 = xl1(q)
+    cpot = rpot(q, Vec3(rl1, 0.0, 0.0))
+    upper = rl1
+    lower = upper / 4
+
+    def root_func(t, dx, dy):
+        """
+        t is a step in direction dx, dy
+        """
+        p = Vec3(t * dx, t * dy, 0.0)
+        return rpot(q, p) - cpot
+
+    gradf = grad(root_func)
+
+    def loop_body(i, state):
+        theta = 2.0 * jnp.pi * i / (n - 1)
+        dx = -jnp.cos(theta)
+        dy = jnp.sin(theta)
+
+        f = partial(root_func, dx=dx, dy=dy)
+        df = partial(gradf, dx=dx, dy=dy)
+        rad = rtsafe(f, df, lower, upper)
+        state["x"] = state["x"].at[i].set(rad * dx)
+        state["y"] = state["y"].at[i].set(rad * dy)
+        return state
+
+    init_state = dict(x=jnp.zeros(n), y=jnp.zeros(n))
+    end_state = fori_loop(0, n, loop_body, init_state)
+    return end_state["x"], end_state["y"]
