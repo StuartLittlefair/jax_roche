a3b6861193b5adceb4d85d7f90cb685fe388df66
Stuart Littlefair <s.littlefair@shef.ac.uk>
2024-02-22 13:35:19 +0000

    GPU enabled RTSAFE

--- jax_roche/methods.py
+++ jax_roche/methods.py
@@ -4,10 +4,13 @@ Numerical Methods for root finding and minimisation.
 Note to self: we cannot jit functions that accept functions as arguments.
 """
 
-from jax.lax import cond, scan
+from jax.lax import cond, scan, while_loop
 from jax import numpy as jnp
+from functools import partial
+from jax import jit
 
 
+@partial(jit, static_argnames=["f", "gradf", "MAXIT", "EPS"])
 def newton(f, gradf, x0, MAXIT=100, EPS=1.0e-8):
     """
     Newton-Raphson Solver
@@ -35,27 +38,21 @@ def newton(f, gradf, x0, MAXIT=100, EPS=1.0e-8):
         x, xold, n = state
         return (n < MAXIT) & (jnp.fabs(x - xold) > EPS * jnp.fabs(x))
 
-    def step_once(state):
+    def step(state):
         x, xold, n = state
         xold = x
         fx = f(x)
         dfx = gradf(x)
         step = fx / dfx
         new_state = x - step, xold, n + 1
-        return new_state, None
-
-    def noop(state):
-        return state, None
-
-    def body(state, _):
-        keep_going = stop_cond(state)
-        return cond(keep_going, step_once, noop, state)
+        return new_state
 
     state = (x0, 0.0, 0)
-    state, _ = scan(body, state, None, length=MAXIT)
+    state = while_loop(stop_cond, step, state)
     return state[0]
 
 
+@partial(jit, static_argnames=["f", "gradf", "MAXIT", "ACC"])
 def rtsafe(f, gradf, lower, upper, MAXIT=100, ACC=1.0e-7):
     """
     Safe root-finding algorithm.
@@ -80,9 +77,12 @@ def rtsafe(f, gradf, lower, upper, MAXIT=100, ACC=1.0e-7):
 
         # update new state
         fx = f(x)
-        xl, xh = cond(fx < 0.0, lambda _: (x, xh), lambda _: (xl, x), None)
+        # xl, xh = cond(fx < 0.0, lambda _: (x, xh), lambda _: (xl, x), None)
+        xl = jnp.where(fx < 0.0, x, xl)
+        xh = jnp.where(fx < 0.0, xh, x)
+
         new_state = x, xold, dx, dxold, n + 1, xh, xl
-        return new_state, None
+        return new_state
 
     def bisect_step(state):
         """
@@ -99,15 +99,19 @@ def rtsafe(f, gradf, lower, upper, MAXIT=100, ACC=1.0e-7):
         fx = f(x)
 
         # update new state
-        xl, xh = cond(fx < 0.0, lambda _: (x, xh), lambda _: (xl, x), None)
+        # xl, xh = cond(fx < 0.0, lambda _: (x, xh), lambda _: (xl, x), None)
+        below = fx < 0.0
+        xl = jnp.where(below, x, xl)
+        xh = jnp.where(below, xh, x)
+
         new_state = x, xold, dx, dxold, n + 1, xh, xl
-        return new_state, None
+        return new_state
 
     def noop(state):
         """
         Does nothing - called when stop_cond is satisfied
         """
-        return state, None
+        return state
 
     def choose_step(state):
         """
@@ -128,7 +132,7 @@ def rtsafe(f, gradf, lower, upper, MAXIT=100, ACC=1.0e-7):
 
         Chooses between N-R and Bisection depending on the position
         """
-        return cond(choose_step(state), bisect_step, newton_step, state,)
+        return cond(choose_step(state), bisect_step, newton_step, state)
 
     def stop_cond(state):
         """
@@ -149,12 +153,16 @@ def rtsafe(f, gradf, lower, upper, MAXIT=100, ACC=1.0e-7):
     # make sure lower and upper limits are the write way around
     # depending on wether f(lower) is +ve or not
     fl = f(lower)
-    xl, xh = cond(
-        fl < 0.0, lambda x: (x[0], x[1]), lambda x: (x[1], x[0]), (lower, upper)
-    )
+    # xl, xh = cond(
+    #    fl < 0.0, lambda x: (x[0], x[1]), lambda x: (x[1], x[0]), (lower, upper)
+    # )
+    xl = jnp.where(fl < 0.0, lower, upper)
+    xh = jnp.where(fl < 0.0, upper, lower)
+
     # make an initial guess
     x0 = 0.5 * (lower + upper)
     dx = dxold = jnp.fabs(upper - lower)
     state = (x0, 0.0, dx, dxold, 0, xh, xl)
-    state, _ = scan(body, state, None, length=MAXIT)
+    # state, _ = scan(body, state, None, length=MAXIT)
+    state = while_loop(stop_cond, take_step, state)
     return state[0]
